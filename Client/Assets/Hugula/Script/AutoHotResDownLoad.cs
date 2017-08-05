a4641c42f4a5c9c87567fa93648afb0e97493a41
RICKY蒲的DELL\puwen <puwenbin@outlook.com>
2017-08-05 23:40:42 +0800

    添加AutoHotResDownLoad脚本。

---
+++ Client/Assets/Hugula/Script/AutoHotResDownLoad.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using Hugula.Loader;
+using Hugula.Update;
+
+public class AutoHotResDownLoad : MonoBehaviour
+{
+    private bool m_isStart = false;
+    // Use this for initialization
+    IEnumerator Start()
+    {
+
+        yield return new WaitForSeconds(1f);
+        if (ManifestManager.CheckNeedBackgroundLoad())
+        {
+            BackGroundDownload.instance.AddBackgroundManifestTask(ManifestManager.fileManifest, null, OnComplete);// AddBackManifestTask
+            BackGroundDownload.instance.Begin();
+        }
+        m_isStart = true;
+
+    }
+
+    void OnComplete(bool isErr)
+    {
+        if (!isErr)
+            ManifestManager.FinishBackgroundLoad();
+    }
+
+    /// <summary>
+    /// This function is called when the object becomes enabled and active.
+    /// </summary>
+    void OnEnable()
+    {
+        if (m_isStart)
+            BackGroundDownload.instance.Begin();
+    }
+
+    /// <summary>
+    /// This function is called when the behaviour becomes disabled or inactive.
+    /// </summary>
+    void OnDisable()
+    {
+        BackGroundDownload.instance.Suspend();
+    }
+
+
+    // /// <summary>
+    // /// suspend download
+    // /// </summary>
+    // public void Suspend()
+    // {
+    //     // enabled = false;
+    //     BackGroundDownload.instance.Suspend();
+    // }
+
+    // /// <summary>
+    // /// begin download
+    // /// </summary>
+    // public void Begin()
+    // {
+    //     BackGroundDownload.instance.Begin();
+    // }
+
+}
